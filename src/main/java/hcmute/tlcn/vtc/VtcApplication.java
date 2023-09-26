3553aebe8391f076fec5fe9d3018c68fdd876021
ToDuyVuong <comc5288@gmail.com>
2023-09-27 00:17:05 +0700

    Create

---
+++ src/main/java/hcmute/tlcn/vtc/VtcApplication.java
@@ -0,0 +1,13 @@
+package hcmute.tlcn.vtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VtcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hcmute/tlcn/vtc/VtcApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmute.tlcn.vtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
