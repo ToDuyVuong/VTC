44368555dde3f69af672999a6b65bf989e57e7ef
ToDuyVuong <comc5288@gmail.com>
2023-09-28 15:56:47 +0700

    Add - Swagger

--- src/main/java/hcmute/tlcn/vtc/VtcApplication.java
+++ src/main/java/hcmute/tlcn/vtc/VtcApplication.java
@@ -3,11 +3,26 @@ package hcmute.tlcn.vtc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class VtcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VtcApplication.class, args);
+        System.out.println("\n\n" );
+
+
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            System.out.println("IP Address: " + ip.getHostAddress());
+            System.out.println("Localhost:  http://localhost:8181/swagger-ui/index.html");
+            System.out.println("\n\n" );
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
 }
