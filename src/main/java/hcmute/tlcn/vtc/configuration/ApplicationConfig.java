2e1bd02eba0c0753652327852d4af8f7dabcc580
ToDuyVuong <comc5288@gmail.com>
2023-10-02 02:54:53 +0700

    Fix - Check username, password by security

--- src/main/java/hcmute/tlcn/vtc/configuration/ApplicationConfig.java
+++ src/main/java/hcmute/tlcn/vtc/configuration/ApplicationConfig.java
@@ -2,11 +2,8 @@ package hcmute.tlcn.vtc.configuration;
 
 import hcmute.tlcn.vtc.auth.AuthenticationService;
 import hcmute.tlcn.vtc.auth.RegisterRequest;
-import hcmute.tlcn.vtc.dto.user.request.RegisterCustomerRequest;
-import hcmute.tlcn.vtc.entity.Customer;
 import hcmute.tlcn.vtc.entity.extra.Role;
 import hcmute.tlcn.vtc.repository.CustomerRepository;
-import hcmute.tlcn.vtc.service.impl.CustomerServiceImpl;
 import hcmute.tlcn.vtc.util.exception.InvalidPasswordException;
 import hcmute.tlcn.vtc.util.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +12,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -34,36 +31,21 @@ public class ApplicationConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-
-//        return username -> {
-//            Customer customer = customerRepository.findByUsername(username)
-//                    .orElseThrow(() -> new NotFoundException("Tài khoản không tồn tại. UserDetailsService"));
-//
-//            // Retrieve the customer's password
-//            String customerPassword = customer.getPassword();
-//
-//            // You will need to obtain the password from the authentication request in a real application
-//            // For demonstration purposes, let's assume the password is passed as a method argument
-//            String providedPassword = "password";  // Replace this with the actual provided password
-//
-//            // Check if the provided password matches the customer's password
-//            if (!passwordEncoder().matches(providedPassword, customerPassword)) {
-//                throw new InvalidPasswordException("Sai mật khẩu");
-//            }
-//
-//            return customer;
-//        };
-
         return username -> customerRepository.findByUsername(username)
-                .orElseThrow(() -> new NotFoundException("Tài khoản không tồn tại. UserDetailsService"));
+                .orElseThrow(() -> new NotFoundException("Tài khoản không tồn tại."));
     }
+
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailsService());
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//        return authProvider;
+//    }
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
+        return new CustomAuthenticationProvider(userDetailsService());
     }
 
     @Bean
@@ -92,10 +74,25 @@ public class ApplicationConfig {
                     .phone("0123456789")
                     .role(Role.CUSTOMER)
                     .build();
-            System.out.println("Admin token: " + service.register(us1));
+            System.out.println("user1 token: " + service.register(us1));
 
 
 
         };
+    }
+
+    public class CustomAuthenticationProvider extends DaoAuthenticationProvider {
+
+        public CustomAuthenticationProvider(UserDetailsService userDetailsService) {
+            setUserDetailsService(userDetailsService);
+            setPasswordEncoder(passwordEncoder());
+        }
+
+        @Override
+        protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) {
+            if (!passwordEncoder().matches(authentication.getCredentials().toString(), userDetails.getPassword())) {
+                throw new InvalidPasswordException("Sai mật khẩu.");
+            }
+        }
     }
 }
