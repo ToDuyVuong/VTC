f0dba0c50389bdcb30fc82493ed2babfa11f9710
ToDuyVuong <comc5288@gmail.com>
2023-11-07 17:33:31 +0700

    Fix CORS by Trung

--- src/main/java/hcmute/tlcn/vtc/configuration/SecurityConfig.java
+++ src/main/java/hcmute/tlcn/vtc/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -78,6 +79,7 @@ public class SecurityConfig {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(req ->
                         req
                                 .requestMatchers(NO_AUTH)
